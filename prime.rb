c069595f16eb8c38717f9d0d75d9837d96451490
Stephen F <stephen@farver.io>
2018-05-27 20:53:07 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -4,6 +4,8 @@ def prime?(integer)
   (2...integer.abs).each do |num|
     if integer.abs == 2 || integer % num == 0 
       prime_flag = false 
+    elsif integer.abs == 2 
+      prime_flag = true
     end
   end
   
