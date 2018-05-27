602d134da7e420e4d6e3a56adb56d7e72cb51054
Stephen F <stephen@farver.io>
2018-05-27 20:41:44 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(integer)
+  range = [2...integer]
+  prime_flag = true 
+  
+  range.each do |num|
+    if integer % num == 0 
+      prime_flag = false 
+    end
+  end
+  
+  prime_flag
+end
